61404eaf9e97e66515b726af934e0c43ea45d02e
VyLe <thaovyletruong@gmail.com>
2020-01-07 14:12:42 -0600

    update search box logic

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,18 +3,19 @@ import "./home.css";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
-  const matchSearch = props.houses.find(house => house.city.toLowerCase() === props.match.city)
-  let all, city;
-  if (matchSearch) { all = "/list/all/"; city = props.submitSearch }
-  else { all = ""; city = "" }
-  console.log(props.submitSearch)
+  // const matchSearch = props.houses.find(house => house.city.toLowerCase() === props.submitSearch.toLowerCase())
+  // let all, city;
+  // if (matchSearch) { all = "/list/all/"; city = props.submitSearch }
+  // else { all = ""; city = ""; }
   return (
     <div>
       <h1>Home</h1>
 
       <input type="text" placeholder="City name" onChange={(e) => props.searchHandle(e)} />
-      <Link to={`${all}${city}`}><button onClick={props.submitSearchHandle}>Submit</button></Link>
-      {all === "" && props.submitSearch ? <h5>Please search for another city</h5> : <h5></h5>}
+      <Link to={`list/all/${props.submitSearch}`}><button onClick={() => props.submitSearchHandle()}>Submit</button></Link>
+      {/* <Link to={`${all}${city}`}><button onClick={() => props.submitSearchHandle()}>Submit</button></Link>
+      {all === "" && props.submitSearch ? <h5>Please search for another city</h5> : null} */}
+
     </div>
   )
 }
