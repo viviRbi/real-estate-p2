455ad8f15fd1c0c56bcaae5055be07a242582d64
VyLe <thaovyletruong@gmail.com>
2020-01-07 15:58:24 -0600

    update search

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,20 +3,19 @@ import "./home.css";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
-  // const matchSearch = props.houses.find(house => house.city.toLowerCase() === props.submitSearch.toLowerCase())
-  // let all, city;
-  // if (matchSearch) { all = "/list/all/"; city = props.submitSearch }
-  // else { all = ""; city = ""; }
+
   return (
     <div>
       <h1>Home</h1>
 
       <input type="text" placeholder="City name" onChange={(e) => props.searchHandle(e)} />
-      <Link to={`list/all/${props.submitSearch}`}><button onClick={() => props.submitSearchHandle()}>Submit</button></Link>
-      {/* <Link to={`${all}${city}`}><button onClick={() => props.submitSearchHandle()}>Submit</button></Link>
-      {all === "" && props.submitSearch ? <h5>Please search for another city</h5> : null} */}
+
+      <Link onClick={() => props.submitSearchHandle()} to={props.houses.find(house => house.city.toLowerCase() === props.submitSearch.toLowerCase()) ? `list/all/${props.submitSearch}` : `/`}>
+        Submit
+      </Link>
 
     </div>
+
   )
 }
 export default Home;
