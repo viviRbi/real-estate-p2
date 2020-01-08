3ea5e2da811f7c7b766487199e10d3e46975c03f
VyLe <thaovyletruong@gmail.com>
2020-01-08 00:34:17 -0600

    update state

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,9 +12,9 @@ const Home = (props) => {
             <h1>Texan Houses</h1>
             <h3>Find your lovely home</h3>
             <input type="text" placeholder="City name" onChange={(e) => props.searchHandle(e)} />
-            <br />
+
             <Link to={props.houses.find(house => house.city.toLowerCase() === props.search.toLowerCase()) ? `list/all/${props.search}` : `/`}>
-              Submit
+              GO
       </Link>
           </div>
         </div>
